2befb69ebb61a82530420639639e37d12627880c
John <john@johnsheets.me>
2017-07-04 07:18:58 -0700

    small refactor

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyC1G5coW3bzIRDso7okgpZimgF7KRUfmzg';
+const API_KEY = '';
 
 class App extends Component {
 	constructor(props) {
